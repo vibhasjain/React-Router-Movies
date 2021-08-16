d3d3863145fad0988b1abe29e81afca94f6327a2
vibes <vibes@vibess-MacBook-Pro.local>
2021-08-16 17:22:20 -0400

    done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams, useRouteMatch, Switch, Route } from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  const { movieID } = useParams();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
+      .get(`http://localhost:5000/api/movies/${movieID}`) // Study this endpoint with Postman
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        setMovie(response.data);
       })
       .catch(error => {
         console.error(error);
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [movieID]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export default function MovieList(props) {
+
+  const { url } = useRouteMatch();
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
+        <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none' }}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
